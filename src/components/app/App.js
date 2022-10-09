00f4901123a0fa938eb071a38d041d27151fff85
millisabel <elena.kozlova.job@gmail.com>
2022-10-09 12:21:08 +0200

    feat: add component EmpoloyersListItem

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,7 @@
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployersListItem from "../employers-list-item/employers-list-item";
 
 import './app.css';
 
@@ -10,7 +12,9 @@ function App() {
 
             <div className="search-panel">
                 <SearchPanel/>
+                <AppFilter/>
             </div>
+            <EmployersListItem/>
         </div>
     );
 }
