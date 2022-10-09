f989b033b2dbee2c8ffc101c5e1a70792ef42071
millisabel <elena.kozlova.job@gmail.com>
2022-10-09 11:27:15 +0200

    feat: add component SearchPanel

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,18 @@
 import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
 
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-      <AppInfo/>
-    </div>
-  );
+    return (
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
