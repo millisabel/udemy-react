dbd3d2934dc31341d73042e944789e41b9f6112c
millisabel <elena.kozlova.job@gmail.com>
2022-11-15 16:57:15 +0100

    update: update style

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,7 +3,7 @@ import './app-info.css';
 const AppInfo = ({increase, employees}) => {
     return (
         <div className='app-info'>
-            <h1>Учет сотрудников в компании</h1>
+            <h1 className='app-title'>Учет сотрудников в компании</h1>
             <h2>Общее число сотрудников: {employees}</h2>
             <h2>Премию получат: {increase}</h2>
         </div>
